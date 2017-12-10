35ea25d381de24191a4bf5d67176755a4350ae23
Alistair Green <alistairmgreen@gmail.com>
2017-12-10 12:13:45 +0000

    Day 10: Refactor to use type u8 instead of usize.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let puzzle_input_lengths: Vec<usize> = vec![
+    let puzzle_input_lengths: Vec<u8> = vec![
         147,
         37,
         249,
@@ -17,21 +17,29 @@ fn main() {
         30,
         70,
     ];
-    let mut numbers: Vec<usize> = (0..256).collect();
+    let mut numbers: Vec<u8> = (0..256).map(|n| n as u8).collect();
 
     knot(&mut numbers, &puzzle_input_lengths);
 
-    let product = numbers[0] * numbers[1];
+    let product = numbers[0] as usize * numbers[1] as usize;
     println!("The product of the first two numbers is {}", product);
 }
 
-fn reverse_segment(numbers: &mut [usize], position: usize, segment_length: usize) {
+fn get_lengths(input: &str) -> Vec<u8> {
+    let mut lengths: Vec<u8> = input.to_string().into_bytes();
+    let mut suffix: Vec<u8> = vec![17, 31, 73, 47, 23];
+    lengths.append(&mut suffix);
+
+    lengths
+}
+
+fn reverse_segment(numbers: &mut [u8], position: usize, segment_length: u8) {
     let numbers_length = numbers.len();
-    let mut reversed: Vec<usize> = numbers
+    let mut reversed: Vec<u8> = numbers
         .iter()
         .cycle()
         .skip(position)
-        .take(segment_length)
+        .take(segment_length as usize)
         .map(|&n| n)
         .collect();
 
@@ -42,14 +50,14 @@ fn reverse_segment(numbers: &mut [usize], position: usize, segment_length: usize
     }
 }
 
-fn knot(numbers: &mut [usize], lengths: &[usize]) {
+fn knot(numbers: &mut [u8], lengths: &[u8]) {
     let mut position = 0usize;
     let mut skip = 0usize;
     let numbers_length = numbers.len();
 
     for &length in lengths {
         reverse_segment(numbers, position, length);
-        position = (position + length + skip) % numbers_length;
+        position = (position + length as usize + skip) % numbers_length;
         skip += 1;
     }
 }
@@ -60,20 +68,25 @@ mod tests {
 
     #[test]
     fn reverse_segment_examples() {
-        let mut numbers: Vec<usize> = (0..5).collect();
+        let mut numbers: Vec<u8> = (0..5).collect();
         reverse_segment(&mut numbers, 0, 3);
-        assert_eq!(numbers, vec![2usize, 1usize, 0usize, 3usize, 4usize]);
+        assert_eq!(numbers, vec![2, 1, 0, 3, 4]);
 
         reverse_segment(&mut numbers, 3, 4);
-        assert_eq!(numbers, vec![4usize, 3usize, 0usize, 1usize, 2usize]);
+        assert_eq!(numbers, vec![4, 3, 0, 1, 2]);
     }
 
     #[test]
     fn knot_example() {
-        let mut numbers: Vec<usize> = (0..5).collect();
-        let lengths: Vec<usize> = vec![3, 4, 1, 5];
+        let mut numbers: Vec<u8> = (0..5).collect();
+        let lengths: Vec<u8> = vec![3, 4, 1, 5];
         knot(&mut numbers, &lengths);
-        let expected: Vec<usize> = vec![3, 4, 2, 1, 0];
-        assert_eq!(numbers, expected);
+        assert_eq!(numbers, vec![3, 4, 2, 1, 0]);
+    }
+
+    #[test]
+    fn get_lengths_converts_string_correctly() {
+        let lengths = get_lengths("1,2,3");
+        assert_eq!(lengths, vec![49, 44, 50, 44, 51, 17, 31, 73, 47, 23]);
     }
 }
