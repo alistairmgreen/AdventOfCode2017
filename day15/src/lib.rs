37d343147df6e41f4a545aa587e31853bb59df29
Alistair Green <alistairmgreen@gmail.com>
2017-12-16 00:26:21 +0000

    Day 15, part 2.

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -33,60 +33,60 @@ pub fn lower_16_bits(number: usize) -> u16 {
     (number & 0xFFFF) as u16
 }
 
+pub fn matches_part_two(seed_a: usize, seed_b: usize) -> usize {
+    let generator_a = Generator::type_a(seed_a).filter(|&n| n % 4 == 0);
+
+    let generator_b = Generator::type_b(seed_b).filter(|&n| n % 8 == 0);
+
+    generator_a
+        .zip(generator_b)
+        .take(5_000_000)
+        .filter(|&(a, b)| lower_16_bits(a) == lower_16_bits(b))
+        .count()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn generator_a_example() {
-        let values: Vec<usize> = Generator::type_a(65)
-            .take(5)
-            .collect();
-        
-        assert_eq!(values,
-            vec![
-                1092455,
-                1181022009,
-                245556042,
-                1744312007,
-                1352636452
-            ]
+        let values: Vec<usize> = Generator::type_a(65).take(5).collect();
+
+        assert_eq!(
+            values,
+            vec![1092455, 1181022009, 245556042, 1744312007, 1352636452]
         );
     }
 
     #[test]
     fn generator_b_example() {
-        let values: Vec<usize> = Generator::type_b(8921)
-            .take(5)
-            .collect();
-        
-        assert_eq!(values,
-            vec![
-                430625591,
-                1233683848,
-                1431495498,
-                137874439,
-                285222916
-            ]
+        let values: Vec<usize> = Generator::type_b(8921).take(5).collect();
+
+        assert_eq!(
+            values,
+            vec![430625591, 1233683848, 1431495498, 137874439, 285222916]
         );
     }
 
     #[test]
     fn lower_16_bits_equal() {
-        assert_eq!(
-            lower_16_bits(245556042),
-            lower_16_bits(1431495498)           
-        );
+        assert_eq!(lower_16_bits(245556042), lower_16_bits(1431495498));
     }
 
     #[test]
     fn example_pairs() {
         let matches = Generator::type_a(65)
-        .zip(Generator::type_b(8921))
-        .take(40_000_000)
-        .filter(|&(a, b)| lower_16_bits(a) == lower_16_bits(b))
-        .count();
+            .zip(Generator::type_b(8921))
+            .take(40_000_000)
+            .filter(|&(a, b)| lower_16_bits(a) == lower_16_bits(b))
+            .count();
 
         assert_eq!(matches, 588);
+    }
+
+    #[test]
+    fn example_part_2() {
+        assert_eq!(matches_part_two(65, 8921), 309);
     }
 }
--- day15/src/main.rs
+++ day15/src/main.rs
@@ -2,11 +2,16 @@ extern crate generators;
 use generators::*;
 
 fn main() {
-    let matches = Generator::type_a(634)
-        .zip(Generator::type_b(301))
+    let seed_a = 634;
+    let seed_b = 301;
+
+    let matches = Generator::type_a(seed_a)
+        .zip(Generator::type_b(seed_b))
         .take(40_000_000)
         .filter(|&(a, b)| lower_16_bits(a) == lower_16_bits(b))
         .count();
     
-    println!("After 40 million iterations, {} pairs were found with matching lower 16 bits.", matches);
+    println!("Part 1: After 40 million iterations, {} pairs were found with matching lower 16 bits.", matches);
+
+    println!("Part 2: After 5 million iterations, {} pairs were found with matching lower 16 bits.", matches_part_two(seed_a, seed_b));
 }
